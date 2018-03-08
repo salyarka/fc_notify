1bd3d95ec300073b7e2b7fac1fb83877fa5a6ae8
Vladimir Levchuk <salyarka@gmail.com>
2018-03-08 17:57:08 +0300

    Added, accept of client connection.

--- server.c
+++ server.c
@@ -11,8 +11,12 @@
 int main(int argc, char *argv[])
 {
 	struct addrinfo hints, *res, *r;
-	int rv, sfd;
+	struct sockaddr_storage c_addr;
+	socklen_t addr_size;
+	int rv, sfd, cfd;
 	int enable = 1;
+	char c_addr_string[INET6_ADDRSTRLEN];
+	void *src;
 
 	memset(&hints, 0, sizeof(hints)); // zero the structure
 	hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6
@@ -59,5 +63,30 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	while(1) {
+		addr_size = sizeof(c_addr);
+		cfd = accept(sfd, (struct sockaddr *)&c_addr, &addr_size);
+
+		if (cfd < 0) {
+			perror("Cant accept connection");
+			continue;
+		}
+		// TODO: client addr dont prints
+		get_client_addr(c_addr, c_addr_string,
+				sizeof(c_addr_string));
+		printf("Got connection from %s\n", c_addr_string);
+	}
+
 	return 0;
+}
+
+// get text presentation of client address
+void get_client_addr(const struct sockaddr *addr, char *s, size_t s_size) {
+	if (addr->sa_family == AF_INET) {
+		inet_ntop(AF_INET, &(((struct sockaddr_in *)addr)->sin_addr),
+				s, s_size);
+	} else if (addr->sa_family == AF_INET6) {
+		inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)addr)->sin6_addr),
+				s, s_size);
+	}
 }
