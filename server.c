9b4bde0b2733e897b25b310b80c6eac3dccc2df3
Vladimir Levchuk <salyarka@gmail.com>
2018-03-18 01:24:21 +0300

    Added, register epoll event for server socket EPOLLIN, redesign receiving of client address, getnameinfo instead of inet_ntop.

--- server.c
+++ server.c
@@ -8,106 +8,142 @@
 #include <arpa/inet.h>
 #include <sys/epoll.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define PORT "59599"
+#define MAX_EV 10
 
+/*
+ *TODO:
+ * - add description
+ * - think, ???setnonblock function instead of exit
+ *   use return -1 and make abort ???
+ * - redesign variables in main function
+ * - register for EPOLLIN event for client socket
+ * */
+
+
+// makes file non blocking
 void setnonblock(int fd)
 {
-	int f;
+    int f;
 
-	if ((f = fcntl(fd, F_GETFL)) < 0) {
-		perror("Cant get flags");
-		exit(1);
-	}
-	f |= O_NONBLOCK;
-	if (fcntl(fd, F_SETFL, f) < 0) {
-		perror("Cant set NONBLOCK");
-		exit(1);
-	}
+    if ((f = fcntl(fd, F_GETFL)) < 0) {
+        perror("Cant get flags");
+        exit(1);
+    }
+    f |= O_NONBLOCK;
+    if (fcntl(fd, F_SETFL, f) < 0) {
+        perror("Cant set NONBLOCK");
+        exit(1);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	struct addrinfo hints, *res, *r;
-	struct sockaddr_storage c_addr;
-	socklen_t addr_size;
-	int rv, sfd, cfd;
-	int enable = 1;
-	char c_ip[INET6_ADDRSTRLEN];
-	int c_port;
-	void *src;
+    struct addrinfo hints, *res, *r;
+    struct sockaddr_storage c_addr;
+    struct epoll_event ev, events[MAX_EV];
+    socklen_t addr_size;
+    int rv, sfd, cfd, c_port, efd, en, i, enable = 1;
+    char c_ip[INET6_ADDRSTRLEN];
+    char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+    void *src;
 
-	memset(&hints, 0, sizeof(hints)); // zero the structure
-	hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use current IP
+    memset(&hints, 0, sizeof(hints)); // zero the structure
+    hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use current IP
 
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &res)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &res)) != 0) {
+	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+	return 1;
+    }
 
-	for (r = res; r != NULL; r = r->ai_next) {
-		// creating server socket
-		if ((sfd = socket(r->ai_family, r->ai_socktype,
-						r->ai_protocol)) < 0) {
-			perror("Cant create server socket");
-			continue;
-		}
+    for (r = res; r != NULL; r = r->ai_next) {
+	    // creating server socket
+	    if ((sfd = socket(r->ai_family, r->ai_socktype,	r->ai_protocol)) < 0) {
+            perror("Cant create server socket");
+            continue;
+        }
 
-		// make the socket reusable
-		if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &enable,
-					sizeof(int)) < 0) {
-			perror("Cant setsockopt");
-			exit(1);
-		}
+        // make the socket reusable
+        if (setsockopt(sfd, SOL_SOCKET,
+                    SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+        perror("Cant setsockopt");
+        exit(1);
+        }
 
-		// bind socket to address
-		if (bind(sfd, r->ai_addr, r->ai_addrlen) < 0) {
-			close(sfd);
-			perror("Cant bind server socket");
-			continue;
-		}
+        // bind socket to address
+        if (bind(sfd, r->ai_addr, r->ai_addrlen) < 0) {
+            close(sfd);
+            perror("Cant bind server socket");
+            continue;
+        }
 
-		break;
-	}
+        break;
+    }
 
-	freeaddrinfo(res);
-	if (r == NULL) {
-		fprintf(stderr, "Failed to bind server socket\n");
-		exit(1);
-	}
+    freeaddrinfo(res);
 
-	if (listen(sfd, 128) < 0) {
-		perror("Cant listen connections");
-		exit(1);
-	}
+    if (r == NULL) {
+        fprintf(stderr, "Failed to bind server socket\n");
+        exit(1);
+    }
 
-	//setnonblock(sfd);
+    if (listen(sfd, 128) < 0) {
+        perror("Cant listen connections");
+        exit(1);
+    }
 
-	while(1) {
-		addr_size = sizeof(c_addr);
-		cfd = accept(sfd, (struct sockaddr *)&c_addr, &addr_size);
+    setnonblock(sfd);
 
-		if (cfd < 0) {
-			perror("Cant accept connection");
-			continue;
-		}
+    if ((efd = epoll_create1(0)) < 0) {
+        perror("Cant create epoll instance");
+        exit(1);
+    }
 
-		// get info about client address
-		if (c_addr.ss_family == AF_INET) {
-			struct sockaddr_in *tmp = (struct sockaddr_in *)&c_addr;
-			c_port = ntohs(tmp->sin_port);
-			inet_ntop(AF_INET, &(tmp->sin_addr), c_ip, sizeof(c_ip));
-		} else {
-			struct sockaddr_in6 *tmp = (struct sockaddr_in6 *)&c_addr;
-			c_port = ntohs(tmp->sin6_port);
-			inet_ntop(AF_INET6, &(tmp->sin6_addr), c_ip, sizeof(c_ip));
-		}
+    ev.data.fd = sfd;
+    ev.events = EPOLLIN;
+    if (epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &ev) < 0) {
+        perror("Cant register server socket on the epoll instance");
+        exit(1);
+    }
 
-		printf("Got connection from %s %d\n", c_ip, c_port);
-	}
+    while(1) {
+        // wait for register events
+        if ((en = epoll_wait(efd, events, MAX_EV, -1)) < 0) {
+            perror("epoll_wait");
+            exit(1);
+        }
 
-	return 0;
+        for (i = 0; i < en; i++) {
+            // got event from server socket
+            if (events[i].data.fd == sfd) {
+                if ((cfd = accept(sfd,
+                        (struct sockaddr *)&c_addr, &addr_size)) < 0) {
+                    // accepted all incoming connections
+                    if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+                        break;
+                    } else {
+                        perror("Cant acept connection");
+                        continue;
+                    }
+                }
+
+                // print info about connected client
+                if ((getnameinfo(
+                        (struct sockaddr *)&c_addr, addr_size,
+                        hbuf, sizeof(hbuf), sbuf, sizeof(sbuf),
+                        NI_NUMERICHOST | NI_NUMERICSERV)) == 0) {
+                    printf("Got connection from %s %s\n", hbuf, sbuf);
+                }
+
+                setnonblock(cfd);
+            }
+        }
+    }
+
+    return 0;
 }
 
