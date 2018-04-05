b59cf8e1e0a47433e55e775517efb5f9030e9665
Vladimir Levchuk <salyarka@gmail.com>
2018-04-06 01:05:40 +0300

    Added, header file.

---
+++ fcn.h
@@ -0,0 +1,22 @@
+#define BUF_SIZE 512
+
+/* 
+ * TODO
+ * reasign BUF_SIZE length
+ * */
+
+// types of messages
+enum msg_type {
+    // registration in server with directory,
+    // where the creation of new files is tracked
+    REGISTRATION,
+    // notification to the server about new file in directory
+    NOTIFY,
+    // subscription for the notification about new files in clients directories
+    SUBSCRIPTION
+}
+
+struct request {
+    char msg[BUF_SIZE];
+    enum msg_type type;
+}
